24368156b3f9192f20aa43b6c1cfa8eda05de1c3
Marianne_Rario <mariannecastillorario27@gmail.com>
2022-01-05 16:56:31 +0800

    inital commit

---
+++ footer.php
@@ -0,0 +1,4 @@
+<h4>Footer</h4>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ single.php
@@ -0,0 +1,10 @@
+<?php
+// querying single content
+get_header();
+while(have_posts()){
+    the_post(); ?>
+    <h2> <?php the_title() ?> </h2>
+    <?php the_content() ?>
+<?php } 
+get_footer();
+?>
